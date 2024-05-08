d6bb704b4e02d41197575749e377a08ba29b7170
SerikSeri <106395383+SerikSeri@users.noreply.github.com>
2024-05-08 22:36:54 +0500

    Add files via upload

---
+++ orders.php
@@ -0,0 +1,89 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Заказдар</title>
+   
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php'; ?>
+
+<section class="orders">
+
+   <h1 class="heading">Орналастырылған заказдар.</h1>
+
+   <div class="box-container">
+
+   <?php
+      if($user_id == ''){
+         echo '<p class="empty">please login to see your orders</p>';
+      }else{
+         $select_orders = $conn->prepare("SELECT * FROM `orders` WHERE user_id = ?");
+         $select_orders->execute([$user_id]);
+         if($select_orders->rowCount() > 0){
+            while($fetch_orders = $select_orders->fetch(PDO::FETCH_ASSOC)){
+   ?>
+   <div class="box">
+      <p>Орналастырылған : <span><?= $fetch_orders['placed_on']; ?></span></p>
+      <p>Аты : <span><?= $fetch_orders['name']; ?></span></p>
+      <p>Email : <span><?= $fetch_orders['email']; ?></span></p>
+      <p>Ұялы номер : <span><?= $fetch_orders['number']; ?></span></p>
+      <p>Мекен жай : <span><?= $fetch_orders['address']; ?></span></p>
+      <p>Төлем : <span><?= $fetch_orders['method']; ?></span></p>
+      <p>Сіздің заказыныз : <span><?= $fetch_orders['total_products']; ?></span></p>
+      <p>Жалпы бағасы : <span>Тг.<?= $fetch_orders['total_price']; ?>/-</span></p>
+      <p> Төлем статусы : <span style="color:<?php if($fetch_orders['payment_status'] == 'pending'){ echo 'red'; }else{ echo 'green'; }; ?>"><?= $fetch_orders['payment_status']; ?></span> </p>
+   </div>
+   <?php
+      }
+      }else{
+         echo '<p class="empty">әзірге тапсырыс берілген жоқ!</p>';
+      }
+      }
+   ?>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
